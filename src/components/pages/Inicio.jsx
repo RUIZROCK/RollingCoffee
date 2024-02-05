48f9d0fde6300b10952c98e5999c95d4088a39cd
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-05 18:38:51 -0300

    Se realizo el componente Inicio con el componente CardProducto agregado en la misma

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,9 +1,25 @@
+import { Container } from "react-bootstrap";
+import CardProducto from "./CardProducto";
+import BannerInicio from "../../assets/bannerInicio.png"
 const Inicio = () => {
-    return (
-        <div>
-            <p>Inicio</p>
-        </div>
-    );
+  return (
+    <>
+    <img src={BannerInicio} alt="Banner RollingCoffee" />
+    <Container className="mainContainer">
+      <section>
+        <h2 className="display-3">Nuestros productos</h2>
+        <hr />
+        <article className="row d-flex justify-content-around">
+          <CardProducto className="col-12 col-sm-12- col-md-3 col-lg-3"></CardProducto>
+          <CardProducto className="col-12 col-sm-12- col-md-3 col-lg-3"></CardProducto>
+          <CardProducto className="col-12 col-sm-12- col-md-3 col-lg-3"></CardProducto>
+          <CardProducto className="col-12 col-sm-12- col-md-3 col-lg-3"></CardProducto>
+        </article>
+      </section>
+    </Container>
+    </>
+    
+  );
 };
 
 export default Inicio;
