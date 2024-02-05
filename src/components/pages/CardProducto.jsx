c136e51fd2596aa744c1110f8bb7d08ad49cc4f8
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-05 18:38:05 -0300

    se realizo el componente CardProducto para la realizacion de componente Inicio

---
+++ src/components/pages/CardProducto.jsx
@@ -0,0 +1,22 @@
+import { Button, Card } from 'react-bootstrap';
+import cafeAmericano from '../../assets/cafeAmericano.png'
+const CardProducto = () => {
+    return (
+        <Card style={{ width: '18rem' }} className='m-1'>
+          <Card.Img variant="top" src={cafeAmericano} />
+          <Card.Body>
+            <Card.Title className='txt-verdecito'>Titulo</Card.Title>
+            <Card.Text>
+              <p>Caracteristicas del producto</p>
+              <b>Precio: $150</b>
+            </Card.Text>
+            <hr />
+            <div className='d-flex justify-content-end'>
+                <Button variant="success">Ver mas</Button>  
+            </div>
+          </Card.Body>
+        </Card>
+    );
+};
+
+export default CardProducto;
