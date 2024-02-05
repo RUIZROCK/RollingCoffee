3dfe3d1eed1ebbf0938f98354717639d54f059dd
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-05 18:20:28 -0300

    realizado la estructura de el menu

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,21 +1,30 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import logo from '../../assets/Coffee_Logo.png'
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "../../assets/Coffee_Logo.png";
 function Menu() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container> 
-        <Navbar.Brand href="#home"><img className='img-fluid' width={120} src={logo} alt="Logo RollingCoffee" /></Navbar.Brand>
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            className="img-fluid"
+            width={120}
+            src={logo}
+            alt="Logo RollingCoffee"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#link">Administrador</Nav.Link>
-            <Nav.Link href="#link">Registro</Nav.Link>
-            <Nav.Link href="#link">Login</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <div className="d-flex justify-content-end">
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Inicio</Nav.Link>
+              <Nav.Link href="#link">Administrador</Nav.Link>
+              <Nav.Link href="#link">Registro</Nav.Link>
+              <Nav.Link href="#link">Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
