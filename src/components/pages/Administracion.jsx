d753d778351de524fb57f61d7f05045cf9cfcd10
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-06 08:31:56 -0300

    Se realizo el enmaquetado del componente Administracion sin parte logica

--- src/components/pages/Administracion.jsx
+++ src/components/pages/Administracion.jsx
@@ -1,9 +1,41 @@
+import { Container } from "react-bootstrap";
+import capu from "../../assets/capuchino.png";
 const Administracion = () => {
-    return (
-        <div>
-            <p>admin</p>            
-        </div>
-    );
+  return (
+    <Container className="mainContainer">
+      <div className="table-responsive">
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Cod</th>
+              <th>Producto</th>
+              <th>Precio</th>
+              <th>Url Img</th>
+              <th>Categoria</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Capuccino</td>
+              <td>$150</td>
+              <td>
+                <div className="middle-item">
+                  <img className="img-fluid" src={capu} alt="Capuchino" />
+                </div>
+              </td>
+              <td>Nose</td>
+              <td>
+                <button className="btn btn-success">Editar</button>
+                <button className="btn btn-danger">Borrar</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </Container>
+  );
 };
 
 export default Administracion;
