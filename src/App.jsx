c57383dbce14dc9d3eaf9039470b58c230f84b1a
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-06 10:41:06 -0300

    Se agrego router dom y luego se aplico en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import './App.css'
 import Footer from './components/common/Footer'
 import Menu from './components/common/Menu'
 import Inicio from './components/pages/Inicio'
-
+import { BrowserRouter, Route,Routes } from 'react-router-dom'
+import Administracion from './components/pages/Administracion'
+import Error404 from './components/pages/Error404'
 function App() {
   return (
-    <>
+    <BrowserRouter>
     <Menu></Menu>
-    <Inicio></Inicio>
+    <Routes>
+      <Route exact path='/' element={<Inicio></Inicio>}></Route>
+      <Route exact path='/Administracion' element={<Administracion></Administracion>}></Route>
+      <Route path='*' element={<Error404></Error404>}></Route>
+    </Routes>
     <Footer></Footer>
-    </>
+    </BrowserRouter>
   )
 }
 
