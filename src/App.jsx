477acc2055a1ec521d498c760af2337555e96651
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-07 21:05:55 -0300

    Se agregaron los componentes detalle y nuevo producto aplicando react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Inicio from './components/pages/Inicio'
 import { BrowserRouter, Route,Routes } from 'react-router-dom'
 import Administracion from './components/pages/Administracion'
 import Error404 from './components/pages/Error404'
+import NuevoProducto from './components/pages/NuevoProducto'
+import DetalleProducto from './components/pages/DetalleProducto'
 function App() {
   return (
     <BrowserRouter>
@@ -13,6 +15,8 @@ function App() {
     <Routes>
       <Route exact path='/' element={<Inicio></Inicio>}></Route>
       <Route exact path='/Administracion' element={<Administracion></Administracion>}></Route>
+      <Route exact path='/NuevoProducto' element={<NuevoProducto></NuevoProducto>}></Route>
+      <Route exact path='/DetalleProducto' element={<DetalleProducto></DetalleProducto>}></Route>
       <Route path='*' element={<Error404></Error404>}></Route>
     </Routes>
     <Footer></Footer>
