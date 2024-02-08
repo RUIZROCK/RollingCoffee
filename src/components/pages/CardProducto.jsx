5d38d6e78b00b48e5e7850c7e98fa08a930e6106
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-07 21:09:07 -0300

    se aplico react router dom a los botones ver mas para derivar al componente detalleProducto

--- src/components/pages/CardProducto.jsx
+++ src/components/pages/CardProducto.jsx
@@ -1,22 +1,25 @@
-import { Button, Card } from 'react-bootstrap';
-import cafeAmericano from '../../assets/cafeAmericano.png'
+import { Card } from "react-bootstrap";
+import cafeAmericano from "../../assets/cafeAmericano.png";
+import { NavLink } from "react-router-dom";
 const CardProducto = () => {
-    return (
-        <Card style={{ width: '18rem' }} className='m-1'>
-          <Card.Img variant="top" src={cafeAmericano} />
-          <Card.Body>
-            <Card.Title className='txt-verdecito'>Titulo</Card.Title>
-            <Card.Text>
-              <p>Caracteristicas del producto</p>
-              <b>Precio: $150</b>
-            </Card.Text>
-            <hr />
-            <div className='d-flex justify-content-end'>
-                <Button variant="success">Ver mas</Button>  
-            </div>
-          </Card.Body>
-        </Card>
-    );
+  return (
+    <Card style={{ width: "18rem" }} className="m-1">
+      <Card.Img variant="top" src={cafeAmericano} />
+      <Card.Body>
+        <Card.Title className="txt-verdecito">Titulo</Card.Title>
+        <Card.Text>
+          <p>Caracteristicas del producto</p>
+          <b>Precio: $150</b>
+        </Card.Text>
+        <hr />
+        <div className="d-flex justify-content-end">
+          <NavLink className="btn btn-success" end to="/DetalleProducto">
+            Ver mas
+          </NavLink>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default CardProducto;
