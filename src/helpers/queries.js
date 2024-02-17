91ef0e5bf14ff1c102a6d7190f1b73eb71fc91c5
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-17 04:21:40 -0300

    se agrego la funcion nuevoProductoAPI

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,3 +1,19 @@
 const URI_Producto = import.meta.env.VITE_API_PRODUCTO;
 
 console.log(URI_Producto);
+
+const nuevoProductoAPI = async (producto)=>{
+    try{
+        const respuesta= await fetch(URI_Producto, {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(producto)
+        });
+        console.log(respuesta);
+        return respuesta;
+    }catch(error){
+        console.log(error);
+    }
+}
